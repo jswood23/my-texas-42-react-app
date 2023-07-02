623a421f8576b896ad3e4443e937472e31647bb2
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-01 23:54:10 -0500

    frontend file cleanup and configure Amplify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify';
+import config from './config';
+
+Amplify.configure({
+  Auth: {
+      region: config.cognito.REGION,
+      userPoolId: config.cognito.USER_POOL_ID,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+      userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+      endpoints: [
+      {
+          name: "notes",
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION
+      },
+      ]
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Howdy there!    
     </div>
   );
 }
