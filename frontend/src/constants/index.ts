591f7fef28d300997c4a0a677cabe25cd371f9ca
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-04 11:43:01 -0500

    set default profile data

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,6 +1,20 @@
-import { type UserData } from '../types'
+import type { ProfileData, UserData } from '../types'
 
 export const requireLoginPages = ['/profile']
+
+export const defaultProfileData: ProfileData = {
+  game_history: [],
+  games_played: 0,
+  games_won: 0,
+  rounds_played: 0,
+  rounds_won: 0,
+  total_points_as_bidder: 0,
+  total_rounds_as_bidder: 0,
+  total_points_as_support: 0,
+  total_rounds_as_support: 0,
+  total_points_as_counter: 0,
+  total_rounds_as_counter: 0
+}
 
 export const defaultUserData: UserData = {
   exists: false,
