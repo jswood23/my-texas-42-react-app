f76b4f4ff4f1d25114bba2cb0fd4ed4674a0a0c1
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-04 12:45:07 -0500

    add api call to get profile data

--- frontend/src/pages/users/profile/index.tsx
+++ frontend/src/pages/users/profile/index.tsx
@@ -1,7 +1,10 @@
+import { API } from 'aws-amplify'
+import { apiContext, defaultProfileData } from '../../../constants'
 import type { OpenAlert, UserData } from '../../../types'
 import { useLocation } from 'react-router-dom'
 import PageContainer from '../../../shared/page-container'
 import queryString from 'query-string'
+import * as React from 'react'
 
 interface Props {
   openAlert: OpenAlert
@@ -11,13 +14,43 @@ interface Props {
 const ProfilePage = ({ openAlert, userData }: Props) => {
   const location = useLocation()
   const queryParams = queryString.parse(location.search)
-  const username: string = (queryParams.username as string) ?? ''
+
+  const [isError, setIsError] = React.useState(false)
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [profileData, setProfileData] = React.useState(defaultProfileData)
+  const [username, setUsername] = React.useState((queryParams.username as string) ?? '')
+
   const pageHeader = username.length ? `${username}'s Profile` : 'User not found'
 
-  return (
-    <PageContainer openAlert={openAlert} title={pageHeader} userData={userData}>
-    </PageContainer>
-  )
+  React.useEffect(() => {
+    const getProfileData = async () => {
+      setIsError(false)
+      setIsLoading(true)
+      await API.get(apiContext, `/users/${username}`, {})
+        .then((response) => {
+          setProfileData(response)
+        }).catch((error) => {
+          console.log(error)
+
+          setIsError(true)
+          setUsername('')
+
+          openAlert('There was an error getting the profile.', 'error')
+        })
+      setIsLoading(false)
+    }
+    getProfileData()
+  }, [location])
+
+  return isError
+    ? (
+        <PageContainer title={pageHeader} openAlert={openAlert} userData={userData} />
+      )
+    : (
+        <PageContainer isLoading={isLoading} title={pageHeader} openAlert={openAlert} userData={userData}>
+          {profileData.games_played}
+        </PageContainer>
+      )
 }
 
 export default ProfilePage
