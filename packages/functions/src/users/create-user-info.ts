647e09ccfe2c966a779635e2526f4409d935f0ed
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-03 20:52:03 -0500

    add checks for bad data or existing entry

--- packages/functions/src/users/create-user-info.ts
+++ packages/functions/src/users/create-user-info.ts
@@ -5,10 +5,35 @@ import dynamoDB from "@my-texas-42-react-app/core/dynamodb";
 export const main = handler(async (event: any) => {
     const data = JSON.parse(event.body);
 
-    const params = {
+    const user_id = event.requestContext.authorizer.iam.cognitoIdentity.identityId;
+
+    // check for missing or invalid values
+    if (!data.username || !data.email) {
+        throw new Error('Missing or invalid user data.');
+    }
+
+    // check for existing userInfo
+    const getParams = {
+        TableName: Table.UserInfo.tableName,
+        KeyConditionExpression: "user_id = :user_id",
+        ExpressionAttributeValues: {
+            ":user_id": user_id,
+        },
+    };
+
+    const result = await dynamoDB.query(getParams);
+
+    if (result.Items) {
+        if (result.Items.length > 0) {
+            throw new Error('A user info entity already exists for this user.');
+        }
+    }
+
+    // create new entry
+    const putParams = {
         TableName: Table.UserInfo.tableName,
         Item: {
-            user_id: data.user_id,
+            user_id,
             username: data.username,
             email: data.email,
             is_admin: false,
@@ -28,7 +53,7 @@ export const main = handler(async (event: any) => {
         },
     };
 
-    await dynamoDB.put(params);
+    await dynamoDB.put(putParams);
 
-    return params.Item;
+    return putParams.Item;
 });
