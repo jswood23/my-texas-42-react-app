21ed8fe99ffc1d50a0120e54e73ae7f17666846b
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-04 18:41:05 -0500

    added mui table and links for usernames

--- frontend/src/shared/profile-friends/index.tsx
+++ frontend/src/shared/profile-friends/index.tsx
@@ -1,3 +1,5 @@
+// import { TableContainer } from '@mui/material'
+import { Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import type { ProfileData } from '../../types'
 
 interface Props {
@@ -5,14 +7,45 @@ interface Props {
 }
 
 const ProfileFriends = ({ profileData }: Props) => {
+  const { friends } = profileData
+  const requests = profileData.incoming_friend_requests
+
+  const getUserRow = (username: string, isFriend: boolean) => {
+    return (
+      <TableRow hover>
+        <TableCell>
+          <Link href={`/profile?username=${username}`}>
+            {username}
+          </Link>
+        </TableCell>
+      </TableRow>
+    )
+  }
+
+  console.log(profileData)
+
+  const listFriends = () =>
+    friends?.map((friendUsername) => getUserRow(friendUsername, true))
+
+  const listRequests = () =>
+    requests?.map((friendUsername) => getUserRow(friendUsername, false))
+
   return (
     <div>
-        <div>
-            <h3>Friends</h3>
-        </div>
-        <div>
-            <h3>Friend Requests</h3>
-        </div>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Friends</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listFriends()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <h3>Friend Requests</h3>
+      {listRequests()}
     </div>
   )
 }
