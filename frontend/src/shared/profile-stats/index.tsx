949e85ce333abf617952e07d84cff0621d2ef74b
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-04 17:42:52 -0500

    display data in profile stats component

--- frontend/src/shared/profile-stats/index.tsx
+++ frontend/src/shared/profile-stats/index.tsx
@@ -5,11 +5,33 @@ interface Props {
 }
 
 const ProfileStats = ({ profileData }: Props) => {
+  const getRatio = (numerator: number, denominator: number, perc = false) => {
+    if (denominator === 0) return '0.0'
+    let ratio = numerator / denominator
+    if (perc) ratio *= 100
+    return ratio.toFixed(1).toString()
+  }
+
+  const getPercentage = (numerator: number, denominator: number) => {
+    const perc = getRatio(numerator, denominator, true)
+    if (perc === '0.0') return '0.0%'
+    return `${perc}%`
+  }
+
+  const gameWinRate = getPercentage(profileData.games_won, profileData.games_played)
+  const roundWinRate = getPercentage(profileData.rounds_won, profileData.rounds_played)
+  const bidderPPR = getRatio(profileData.total_points_as_bidder, profileData.total_rounds_as_bidder)
+  const supportPPR = getRatio(profileData.total_points_as_support, profileData.total_rounds_as_support)
+  const counterPPR = getRatio(profileData.total_points_as_counter, profileData.total_rounds_as_counter)
+
   return (
     <div>
         <h2>Game Stats</h2>
-        <p>Games Played: {profileData.games_played}</p>
-        <p>Games Won: {profileData.games_won}</p>
+        <p>Game win rate: {gameWinRate}</p>
+        <p>Round win rate: {roundWinRate}</p>
+        <p>Bidder PPR: {bidderPPR}</p>
+        <p>Support PPR: {supportPPR}</p>
+        <p>Counter PPR: {counterPPR}</p>
     </div>
   )
 }
