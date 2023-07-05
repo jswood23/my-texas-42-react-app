d1865827b18cd02935aaf47aa7dea93d763cca9f
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-04 19:40:19 -0500

    added styling and empty list message

--- frontend/src/shared/profile-friends/index.tsx
+++ frontend/src/shared/profile-friends/index.tsx
@@ -1,6 +1,24 @@
 // import { TableContainer } from '@mui/material'
 import { Link, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import type { ProfileData } from '../../types'
+import styled from 'styled-components'
+
+const StyledRoot = styled.div({
+  display: 'flex',
+  border: '0.5px solid #B0B0B0',
+  borderRadius: '3px',
+  boxShadow: '0 2px 5px 2px #E0E0E0',
+  '.user-container': {
+    maxHeight: '300px',
+    minHeight: '150px'
+  },
+  '.user-link': {
+    textDecoration: 'none'
+  },
+  '.friend-requests': {
+    borderLeft: '0.5px solid #B0B0B0'
+  }
+})
 
 interface Props {
   profileData: ProfileData
@@ -12,9 +30,12 @@ const ProfileFriends = ({ profileData }: Props) => {
 
   const getUserRow = (username: string, isFriend: boolean) => {
     return (
-      <TableRow hover>
+      <TableRow
+        hover
+        key={`user-${username}`}
+      >
         <TableCell>
-          <Link href={`/profile?username=${username}`}>
+          <Link className='user-link' href={`/profile?username=${username}`}>
             {username}
           </Link>
         </TableCell>
@@ -22,18 +43,36 @@ const ProfileFriends = ({ profileData }: Props) => {
     )
   }
 
-  console.log(profileData)
+  const listFriends = () => {
+    if (!friends?.length) {
+      return (
+        <TableRow>
+          <TableCell>
+            No friends, very sad :(
+          </TableCell>
+        </TableRow>
+      )
+    }
+    return friends?.map((friendUsername) => getUserRow(friendUsername, true))
+  }
 
-  const listFriends = () =>
-    friends?.map((friendUsername) => getUserRow(friendUsername, true))
-
-  const listRequests = () =>
-    requests?.map((friendUsername) => getUserRow(friendUsername, false))
+  const listRequests = () => {
+    if (!requests?.length) {
+      return (
+        <TableRow>
+          <TableCell>
+            No incoming friend requests.
+          </TableCell>
+        </TableRow>
+      )
+    }
+    return requests?.map((requestUsername) => getUserRow(requestUsername, false))
+  }
 
   return (
-    <div>
-      <TableContainer>
-        <Table>
+    <StyledRoot>
+      <TableContainer className='user-container'>
+        <Table size='small'>
           <TableHead>
             <TableRow>
               <TableCell>Friends</TableCell>
@@ -44,9 +83,20 @@ const ProfileFriends = ({ profileData }: Props) => {
           </TableBody>
         </Table>
       </TableContainer>
-      <h3>Friend Requests</h3>
-      {listRequests()}
-    </div>
+
+      <TableContainer className='friend-requests user-container'>
+        <Table size='small'>
+          <TableHead>
+            <TableRow>
+              <TableCell>Friend Requests</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listRequests()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </StyledRoot>
   )
 }
 
