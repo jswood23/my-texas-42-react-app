d37d34d229b5c288655ecd69616847559de832a2
jswood23 <jwood@swood.org>
2023-07-05 17:31:15 -0500

    move to styled components in page container

--- frontend/src/shared/page-container/index.tsx
+++ frontend/src/shared/page-container/index.tsx
@@ -1,7 +1,8 @@
 import { CircularProgress, Container } from '@mui/material'
+import { isMobile } from 'react-device-detect'
 import type { OpenAlert, UserData } from '../../types'
-import { THEME } from '../../constants/theme'
 import * as React from 'react'
+import styled from 'styled-components'
 
 interface Props {
   children?: any
@@ -11,34 +12,26 @@ interface Props {
   userData: UserData
 }
 
+const StyledContainer = styled(Container)(({ theme }) => ({
+  // backgroundColor: THEME.palette.secondary.main,
+  backgroundColor: 'white',
+  minHeight: isMobile ? '560px' : '800px',
+  width: '100%',
+  marginTop: theme.spacing(2),
+  padding: theme.spacing(2),
+
+  border: '1px solid #A0A0A0',
+  borderRadius: '5px',
+  boxShadow: '0 2px 5px 3px #E0E0E0'
+}))
+
 const PageContainer = ({ children, isLoading = false, openAlert, title, userData }: Props) => {
-  const [pageHeight, setPageHeight] = React.useState(0)
-
-  React.useEffect(() => {
-    setPageHeight(window.innerHeight - 140)
-  }, [])
-
-  const classes = {
-    backdrop: {
-      // backgroundColor: THEME.palette.secondary.main,
-      backgroundColor: 'white',
-      minHeight: pageHeight,
-      width: '100%',
-      marginTop: THEME.spacing(2),
-      padding: THEME.spacing(2),
-
-      border: '1px solid #A0A0A0',
-      borderRadius: '5px',
-      boxShadow: '0 2px 5px 3px #E0E0E0'
-    }
-  }
-
   return (
-    <Container fixed style={classes.backdrop}>
+    <StyledContainer fixed>
       <h1>{title}</h1>
       <hr />
       {isLoading ? <CircularProgress /> : children}
-    </Container>
+    </StyledContainer>
   )
 }
 
