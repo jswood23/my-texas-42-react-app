57e5e818cb6734f47b9a06e0e5e741ccaad31d55
jswood23 <jwood@swood.org>
2023-07-06 16:33:11 -0500

    add styles and border to profile stats

--- frontend/src/pages/users/profile/profile-stats.tsx
+++ frontend/src/pages/users/profile/profile-stats.tsx
@@ -1,4 +1,23 @@
+import { Typography } from '@mui/material'
 import type { ProfileData } from '../../../types'
+import styled from 'styled-components'
+
+const StyledRoot = styled.div(({ theme }) => ({
+  border: '1px solid #A0A0A0',
+  borderRadius: '5px',
+  boxShadow: '0 2px 5px 3px #E0E0E0',
+  marginBottom: theme.spacing(2),
+  padding: theme.spacing(1),
+  width: '75%',
+  '.stats-title': {
+    borderBottom: '1px solid #A0A0A0',
+    fontSize: theme.spacing(2.5),
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    width: '100%'
+  }
+}))
 
 interface Props {
   profileData: ProfileData
@@ -25,14 +44,14 @@ const ProfileStats = ({ profileData }: Props) => {
   const counterPPR = getRatio(profileData.total_points_as_counter, profileData.total_rounds_as_counter)
 
   return (
-    <div>
-        <h2>Game Stats</h2>
-        <p>Game win rate: {gameWinRate}</p>
-        <p>Round win rate: {roundWinRate}</p>
-        <p>Bidder PPR: {bidderPPR}</p>
-        <p>Support PPR: {supportPPR}</p>
-        <p>Counter PPR: {counterPPR}</p>
-    </div>
+    <StyledRoot>
+      <Typography className="stats-title">Game Stats</Typography>
+      <p>Game win rate: {gameWinRate}</p>
+      <p>Round win rate: {roundWinRate}</p>
+      <p>Bidder PPR: {bidderPPR}</p>
+      <p>Support PPR: {supportPPR}</p>
+      <p>Counter PPR: {counterPPR}</p>
+    </StyledRoot>
   )
 }
 
