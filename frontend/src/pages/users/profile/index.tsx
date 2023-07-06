9b428195b3dea21e588ae1de0e3dd498b671be3d
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-05 22:36:24 -0500

    added profile friend actions to other user profile page

--- frontend/src/pages/users/profile/index.tsx
+++ frontend/src/pages/users/profile/index.tsx
@@ -1,5 +1,7 @@
 import { API } from 'aws-amplify'
 import { apiContext, defaultProfileData } from '../../../constants'
+import { Button, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material'
+import { Cancel, GroupAdd, KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material'
 import { limitString } from '../../../utils/string-utils'
 import type { OpenAlert, UserData } from '../../../types'
 import { useLocation } from 'react-router-dom'
@@ -8,17 +10,32 @@ import ProfileStats from '../../../shared/profile-stats'
 import queryString from 'query-string'
 import * as React from 'react'
 import ProfileFriends from '../../../shared/profile-friends'
+import styled from 'styled-components'
 
 interface Props {
   openAlert: OpenAlert
   userData: UserData
 }
 
+const StyledRoot = styled(Button)(({ theme }) => ({
+  '.profile-friend-actions-button': {
+    color: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.alt,
+    fontWeight: 'bold',
+    '&:hover': {
+      color: theme.palette.secondary.alt,
+      backgroundColor: theme.palette.primary.main
+    }
+  }
+}))
+
 const ProfilePage = ({ openAlert, userData }: Props) => {
   const location = useLocation()
   const queryParams = queryString.parse(location.search)
   const queryUsername = (queryParams.username as string) ?? ''
 
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const isDropdownOpen = Boolean(anchorEl)
   const [isError, setIsError] = React.useState(false)
   const [isLoading, setIsLoading] = React.useState(false)
   const [profileData, setProfileData] = React.useState(defaultProfileData)
@@ -49,12 +66,107 @@ const ProfilePage = ({ openAlert, userData }: Props) => {
     else setIsError(true)
   }, [location])
 
+  const getProfileFriendAction = () => {
+    const isFriends = profileData?.friends?.includes(userData?.username)
+    const isRequested = profileData?.incoming_friend_requests?.includes(userData?.username)
+
+    const handleCloseDropdown = React.useCallback(() => {
+      setAnchorEl(null)
+    }, [setAnchorEl])
+
+    const handleOpenDropdown = (event: any) => {
+      setAnchorEl(event.currentTarget)
+    }
+
+    const handleClickAddFriend = async () => {
+      handleCloseDropdown()
+      await API.get(apiContext, `/friends/send_request/${username}`, {})
+        .then(() => {
+          openAlert(`Sent friend request to ${username}.`, 'success')
+        }).catch((error) => {
+          console.log(error)
+          openAlert('There was an error sending the friend request.', 'error')
+        })
+    }
+
+    const handleClickCancelRequest = async () => {
+      handleCloseDropdown()
+      await API.get(apiContext, `/friends/cancel_request/${username}`, {})
+        .then(() => {
+          openAlert(`Canceled friend request to ${username}.`, 'success')
+        }).catch((error) => {
+          console.log(error)
+          openAlert('There was an error canceling the friend request.', 'error')
+        })
+    }
+
+    const handleClickRemoveFriend = async () => {
+      handleCloseDropdown()
+      await API.get(apiContext, `/friends/remove_friend/${username}`, {})
+        .then(() => {
+          openAlert(`Removed ${username} from friend list.`, 'success')
+        }).catch((error) => {
+          console.log(error)
+          openAlert('There was an error removing this friend.', 'error')
+        })
+    }
+
+    return (isOwnProfile || isLoading)
+      ? <></>
+      : (
+      <StyledRoot>
+        <Button
+          className='profile-friend-actions-button'
+          variant='contained'
+          onClick={handleOpenDropdown}
+          endIcon={
+            isDropdownOpen ? <KeyboardArrowUp /> : <KeyboardArrowDown />
+          }
+        >
+          {isFriends ? <>Friend</> : isRequested ? <>Friend request sent</> : <>Not friends</>}
+        </Button>
+        <Menu
+          id="friends-actions-dropdown-container"
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={isDropdownOpen}
+          onClose={handleCloseDropdown}
+        >
+          {isFriends &&
+            <MenuItem onClick={handleClickRemoveFriend} disableRipple>
+              <ListItemIcon>
+                <Cancel fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Remove friend</ListItemText>
+            </MenuItem>
+          }
+          {!isFriends && !isRequested &&
+            <MenuItem onClick={handleClickAddFriend} disableRipple>
+              <ListItemIcon>
+                <GroupAdd fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Add friend</ListItemText>
+            </MenuItem>
+          }
+          {!isFriends && isRequested &&
+            <MenuItem onClick={handleClickCancelRequest} disableRipple>
+              <ListItemIcon>
+                <Cancel fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Cancel friend Request</ListItemText>
+            </MenuItem>
+          }
+        </Menu>
+      </StyledRoot>
+        )
+  }
+
   return isError
     ? (
         <PageContainer title={pageHeader} openAlert={openAlert} userData={userData} />
       )
     : (
-        <PageContainer isLoading={isLoading} title={pageHeader} openAlert={openAlert} userData={userData}>
+        <PageContainer action={getProfileFriendAction()} isLoading={isLoading} title={pageHeader} openAlert={openAlert} userData={userData}>
           <ProfileStats profileData={profileData} />
           {isOwnProfile &&
             <ProfileFriends openAlert={openAlert} profileData={profileData} userData={userData} />
