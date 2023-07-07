f21750cfefd3797220316968293b8dbfc34e3b62
Owen Dunston <owsoni2000@aol.com>
2023-07-06 20:13:05 -0500

    added friendship API

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -35,6 +35,12 @@ export function ApiStack({ stack, app }: StackContext) {
         'GET /list_lobbies': 'packages/functions/src/lobbies/list-lobbies.main', // list all lobbies available to the user
         // user profiles
         'GET /users/{username}': 'packages/functions/src/users/get-user-profile.main', // get a user's profile information by username
+        // friend requests
+        'GET /friends/send_request/{username}': 'packages/functions/src/friends/add-friend.main', //send friend request
+        'GET /friends/cancel_request/{username}': 'packages/functions/src/friends/cancel-request.main', //cancel friend request
+        'GET /friends/remove_friend/{username}': 'packages/functions/src/friends/remove-friend.main', //remove a friend from friends list
+        'GET /friends/accept_request/{username}': 'packages/functions/src/friends/accept-request.main', //accept a friend request
+        'GET /friends/reject_request/{username}': 'packages/functions/src/friends/reject-request.main', //reject a friend request (ouch)
       },
     });
 
