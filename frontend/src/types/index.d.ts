ef1d5dc952ce73acc8b24ef0506b0f8f57d9a33a
jswood23 <jwood@swood.org>
2023-07-07 11:55:32 -0500

    create lobby info type

--- frontend/src/types/index.d.ts
+++ frontend/src/types/index.d.ts
@@ -1,3 +1,14 @@
+export interface LobbyInfo {
+  match_id: string
+  match_name: string
+  match_invite_code: string
+  match_privacy: number
+  allowed_players: string[]
+  rules: string[]
+  team_1: string[]
+  team_2: string[]
+}
+
 export type OpenAlert = (message: string, severity: string) => void
 
 export interface ProfileData {
