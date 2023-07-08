d81dc47b9ac15126afc3a80f664188f64f4c57c8
Owen Dunston <owsoni2000@aol.com>
2023-07-07 20:39:26 -0500

    fixed bug, removes accepted user from incoming

--- packages/functions/src/friends/accept-request.ts
+++ packages/functions/src/friends/accept-request.ts
@@ -20,11 +20,14 @@ export const main = handler(async (event: any) =>
     const me_fr_fr = event.requestContext.authorizer.iam.cognitoIdentity.amr[2].slice(-36);
     const accepter = await getUserById(me_fr_fr)
 
-    //creates copy of current friend list
+    //creates copy of current friend list and incoming_friend_requests
     let new_list = accepter.friends
+    let new_list2 = accepter.incoming_friend_requests
 
-    //adds new_friend username to that list
+    //adds new_friend username to that list and removes friend request
     new_list.push(new_friend.username)
+    const ind = new_list.indexOf(new_friend.username)
+    const real_list = new_list.splice(ind, 1)
 
     const params=
     {
@@ -32,10 +35,11 @@ export const main = handler(async (event: any) =>
         Key: {
             user_id: accepter.user_id,
         },
-        UpdateExpression: "SET friends = :friends",
+        UpdateExpression: "SET friends = :friends, incoming_friend_requests = :incoming_friend_requests",
         ExpressionAttributeValues: {
             //make databse request to set friends equal to new_list
-            ":friends": new_list
+            ":friends": new_list,
+            ":incoming_friend_requests": real_list,
         },
         ReturnValues: "ALL_NEW",
     };
