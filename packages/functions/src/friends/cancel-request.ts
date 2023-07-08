ca7a4a1c5abf13c9d45496333836e1f115e5cb98
Owen Dunston <owsoni2000@aol.com>
2023-07-07 20:39:52 -0500

    needs testing

--- packages/functions/src/friends/cancel-request.ts
+++ packages/functions/src/friends/cancel-request.ts
@@ -16,4 +16,38 @@ export const main = handler(async (event: any) =>
     //get recipient of request
     const recipient = await getUserByUsername(you);
 
+    //get yourself as string and user object
+    const me_fr_fr = event.requestContext.authorizer.iam.cognitoIdentity.amr[2].slice(-36);
+    const canceller = await getUserById(me_fr_fr)
+
+    //error: cancel request when someone has already accepted it
+    if (recipient.friends.includes(me_fr_fr))
+    {
+        throw new Error("The user has already accepted the friend request")
+    }
+
+    //creates copy of current incoming_friend_request list
+    let new_list = recipient.incoming_friend_requests
+
+    //removes canceller from list by getting the index of name, and splicing list at that value
+    const ind = new_list.indexOf(canceller.username)
+    const real_list = new_list.splice(ind, 1)
+
+    const params=
+    {
+        TableName: Table.UserInfo.tableName,
+        Key: {
+            user_id: canceller.user_id,
+        },
+        UpdateExpression: "SET incoming_friend_requests = :incoming_friend_requests",
+        ExpressionAttributeValues: {
+            //make databse request to set incoming_friend_requests equal to real_list
+            ":incoming_friend_requests": real_list
+        },
+        ReturnValues: "ALL_NEW",
+    };
+    
+    await dynamoDB.update(params);
+    return {status: true};
+
 })
