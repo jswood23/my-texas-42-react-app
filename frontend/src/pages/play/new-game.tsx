21411644a5eb7a2d929c020ecfa2c7dd70cd80d6
jswood23 <jwood@swood.org>
2023-07-10 15:26:50 -0500

    add match name input field

--- frontend/src/pages/play/new-game.tsx
+++ frontend/src/pages/play/new-game.tsx
@@ -1,4 +1,26 @@
+import { FormControl, TextField } from '@mui/material'
 import type { OpenAlert, UserData } from '../../types'
+import { validateField } from '../../utils/user-utils'
+import styled from 'styled-components'
+import React from 'react'
+
+const StyledRoot = styled.div(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  '.form-container': {
+    width: theme.spacing(35),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  '.form-text-input': {
+    width: '100%',
+    marginTop: theme.spacing(3)
+  }
+}))
 
 interface Props {
   onChangeStage: (newStage: string) => void
@@ -7,7 +29,55 @@ interface Props {
 }
 
 const NewGame = ({ onChangeStage, openAlert, userData }: Props) => {
-  return <>Create a new game here</>
+  const [matchName, setMatchName] = React.useState('')
+  const [errors, setErrors] = React.useState({ hasError: false, matchName: null })
+
+  const runValidationTasks = React.useCallback(
+    (fieldName: string, currentValue: string) => {
+      const validations = {
+        matchName: [
+          { type: 'Required' },
+          { type: 'GreaterThanChar', numValues: [5] }
+        ]
+      }
+      const validationResponse = validateField(
+        currentValue,
+        (validations as any)[fieldName]
+      )
+      setErrors((errors) => ({ ...errors, [fieldName]: validationResponse }))
+      return validationResponse
+    },
+    []
+  )
+
+  const onChangeMatchName = (e: { target: { value: string } }) => {
+    const value = e.target.value
+    if ((errors.matchName as any)?.hasError) {
+      runValidationTasks('matchName', value)
+    }
+    setMatchName(value)
+  }
+
+  return (
+    <StyledRoot>
+      <div className="form-container">
+        <FormControl>
+          <TextField
+            label="Match Name"
+            id="match-name"
+            autoComplete="on"
+            size="small"
+            className="form-text-input"
+            value={matchName}
+            onChange={onChangeMatchName}
+            onBlur={() => runValidationTasks('matchName', matchName)}
+            helperText={(errors.matchName as any)?.errorMessage}
+            error={(errors.matchName as any)?.hasError}
+          />
+        </FormControl>
+      </div>
+    </StyledRoot>
+  )
 }
 
 export default NewGame
