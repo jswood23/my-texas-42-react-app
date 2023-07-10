e6866d33cced7d539b9e57f8fee8c60da4507ebd
jswood23 <jwood@swood.org>
2023-07-10 15:45:42 -0500

    add privacy select

--- frontend/src/pages/play/new-game.tsx
+++ frontend/src/pages/play/new-game.tsx
@@ -1,6 +1,7 @@
-import { FormControl, TextField } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, TextField } from '@mui/material'
 import type { OpenAlert, UserData } from '../../types'
 import { validateField } from '../../utils/user-utils'
+import Select, { type SelectChangeEvent } from '@mui/material/Select'
 import styled from 'styled-components'
 import React from 'react'
 
@@ -14,11 +15,13 @@ const StyledRoot = styled.div(({ theme }) => ({
     width: theme.spacing(35),
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: theme.spacing(2)
   },
   '.form-text-input': {
     width: '100%',
-    marginTop: theme.spacing(3)
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(2)
   }
 }))
 
@@ -30,6 +33,7 @@ interface Props {
 
 const NewGame = ({ onChangeStage, openAlert, userData }: Props) => {
   const [matchName, setMatchName] = React.useState('')
+  const [privacy, setPrivacy] = React.useState(0)
   const [errors, setErrors] = React.useState({ hasError: false, matchName: null })
 
   const runValidationTasks = React.useCallback(
@@ -58,22 +62,38 @@ const NewGame = ({ onChangeStage, openAlert, userData }: Props) => {
     setMatchName(value)
   }
 
+  const onChangePrivacy = (e: SelectChangeEvent) => {
+    setPrivacy(+e.target.value)
+  }
+
   return (
     <StyledRoot>
       <div className="form-container">
-        <FormControl>
-          <TextField
-            label="Match Name"
-            id="match-name"
-            autoComplete="on"
+        <TextField
+          label="Match Name"
+          id="match-name"
+          autoComplete="on"
+          size="small"
+          className="form-text-input"
+          value={matchName}
+          onChange={onChangeMatchName}
+          onBlur={() => runValidationTasks('matchName', matchName)}
+          helperText={(errors.matchName as any)?.errorMessage}
+          error={(errors.matchName as any)?.hasError}
+        />
+        <FormControl className="form-text-input">
+          <InputLabel id="privacy-select-label">Privacy</InputLabel>
+          <Select
+            label="Privacy"
+            id="privacy-select"
             size="small"
-            className="form-text-input"
-            value={matchName}
-            onChange={onChangeMatchName}
-            onBlur={() => runValidationTasks('matchName', matchName)}
-            helperText={(errors.matchName as any)?.errorMessage}
-            error={(errors.matchName as any)?.hasError}
-          />
+            value={privacy.toString()}
+            onChange={onChangePrivacy}
+          >
+            <MenuItem value={0}>Public</MenuItem>
+            <MenuItem value={1}>Friends only</MenuItem>
+            <MenuItem value={2}>Invite only</MenuItem>
+          </Select>
         </FormControl>
       </div>
     </StyledRoot>
