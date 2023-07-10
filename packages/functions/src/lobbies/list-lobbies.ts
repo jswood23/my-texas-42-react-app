91f47bc16e966ae18f1a759605e794da22c1e7ae
jswood23 <jwood@swood.org>
2023-07-10 11:42:27 -0500

    update format for new endpoint

--- packages/functions/src/lobbies/list-lobbies.ts
+++ packages/functions/src/lobbies/list-lobbies.ts
@@ -4,7 +4,7 @@ import dynamoDB from '@my-texas-42-react-app/core/dynamodb';
 
 
 export const main = handler(async (event: any) => {
-  const publicLobbies: any[] = []
+  const publicLobbies: any[] = [];
   const privateLobbies: any[] = [];
   return {
     inGame: false,
