913c0537393cd1dc191a2c1ad0a4c584d0180095
jswood23 <jwood@swood.org>
2023-07-12 17:52:12 -0500

    add websocket api url to environment variables

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -13,6 +13,9 @@ const config = {
     USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
     APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
     IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID
+  },
+  websocket: {
+    URL: process.env.REACT_APP_WEBSOCKET_API_PATH
   }
 }
 
