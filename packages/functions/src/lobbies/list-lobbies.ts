fcdb9dabb7a6dfd0286f5ad83ac935fa6aeb1f01
jswood23 <jwood@swood.org>
2023-07-12 13:48:50 -0500

    limit lobby attributes sent to frontend

--- packages/functions/src/lobbies/list-lobbies.ts
+++ packages/functions/src/lobbies/list-lobbies.ts
@@ -3,19 +3,19 @@ import handler from '@my-texas-42-react-app/core/handler';
 import dynamoDB from '@my-texas-42-react-app/core/dynamodb';
 import { getCurrentUser } from 'src/utils/user-utils';
 
-export interface LobbyInfo {
-  match_id: string;
-  match_name: string;
-  match_invite_code: string;
-  match_privacy: number;
-  rules: string[];
-  team_1: string[];
-  team_2: string[];
-}
-
 export const main = handler(async (event: any) => {
   const params = {
     TableName: Table.CurrentMatch.tableName,
+    AttributesToGet: [
+      'match_id',
+      'match_name',
+      'match_invite_code',
+      'match_privacy',
+      'allowed_players',
+      'rules',
+      'team_1',
+      'team_2',
+    ],
   };
 
   const result = await dynamoDB.scan(params);
