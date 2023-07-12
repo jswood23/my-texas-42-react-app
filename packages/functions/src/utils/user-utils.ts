af6a8daf96e04664a1c4ef088554527417ba7115
jswood23 <jwood@swood.org>
2023-07-12 11:10:28 -0500

    add getCurrentUser util function

--- packages/functions/src/utils/user-utils.ts
+++ packages/functions/src/utils/user-utils.ts
@@ -1,6 +1,25 @@
 import { Table } from 'sst/node/table';
 import dynamoDB from '@my-texas-42-react-app/core/dynamodb';
 
+export const getCurrentUser = async (event: any) => {
+  const id = event.requestContext.authorizer.iam.cognitoIdentity.amr[2].slice(-36);
+  
+  const params = {
+    TableName: Table.UserInfo.tableName,
+    Key: {
+      user_id: id,
+    },
+  };
+
+  const result = await dynamoDB.get(params);
+
+  if (!result.Item) {
+    throw new Error('User does not exist.');
+  }
+
+  return result.Item as any;
+}
+
 export const getUserByUsername = async (username: string) => {
   const params = {
     TableName: Table.UserInfo.tableName,
