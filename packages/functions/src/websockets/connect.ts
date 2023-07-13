79e4f994701adc86ab98cb632cfe899cbac24592
jswood23 <jwood@swood.org>
2023-07-13 11:11:56 -0500

    check for valid team number in connect function

--- packages/functions/src/websockets/connect.ts
+++ packages/functions/src/websockets/connect.ts
@@ -5,17 +5,26 @@ import dynamodb from "@my-texas-42-react-app/core/dynamodb";
 import { addConnectionToTable } from "src/utils/websocket-utils";
 
 export const main: APIGatewayProxyHandler = async (event) => {
-  const { user_id, match_invite_code } = event?.queryStringParameters ?? { user_id: '', match_invite_code: ''};
+  const { match_invite_code, user_id } =
+    event?.queryStringParameters ?? {
+      user_id: '',
+      match_invite_code: '',
+    };
   const conn_id = event.requestContext.connectionId ?? '';
+  const teamNumber = +(event?.queryStringParameters?.team_number ?? '0');
 
-  if (!user_id || !match_invite_code || match_invite_code.length !== 6) {
-    return { statusCode: 500, body: 'Missing user id or match code.'};
+  if (
+    !user_id ||
+    !match_invite_code ||
+    match_invite_code.length !== 6 ||
+    !(teamNumber === 1 || teamNumber === 2)
+  ) {
+    return { statusCode: 500, body: 'Missing connection information.' };
   }
 
   const lobby = await getLobbyByInviteCode(match_invite_code);
 
   const { match_id } = lobby;
-
   await addConnectionToTable(conn_id, match_id, user_id);
 
   return { statusCode: 200, body: 'Connected' };
