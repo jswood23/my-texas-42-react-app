d1258e800ac6362011f1cea9502666d334043102
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-15 22:57:51 -0500

    add chat log append

--- packages/functions/src/game-chat/send-game-chat.ts
+++ packages/functions/src/game-chat/send-game-chat.ts
@@ -1,3 +1,4 @@
+import { addToGameChat } from "src/utils/lobby-utils";
 import { getConnectionById, getConnectionsByMatchId, sendToConnections } from "src/utils/websocket-utils"
 import type { APIGatewayProxyEvent } from "aws-lambda";
 
@@ -9,6 +10,13 @@ export const sendGameChat = async (event: APIGatewayProxyEvent) => {
   const conn_ids = await getConnectionsByMatchId(thisConnection.match_id);
 
   const messageData = JSON.parse(JSON.parse((event.body as string)).data as string);
+
+  const chatLogMessage = {
+    message: messageData.message,
+    username: messageData.username
+  };
+
+  await addToGameChat(thisConnection.match_id, JSON.stringify(chatLogMessage));
 
   await sendToConnections(event, thisConnection.match_id, messageData, conn_ids);
 }
