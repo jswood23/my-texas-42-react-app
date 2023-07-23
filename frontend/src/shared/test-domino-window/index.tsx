429cab0354f9855fff6d1d387c10b104038a7efa
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-07-23 16:53:53 -0500

    update domino window

--- frontend/src/shared/test-domino-window/index.tsx
+++ frontend/src/shared/test-domino-window/index.tsx
@@ -1,28 +1,53 @@
 import { type DominoPlacement } from '../../types'
 import Domino from '../domino'
+import React from 'react'
 import styled from 'styled-components'
 
-const DominoWindow = () => {
-  const centerX = 150
-  const centerY = 200
+const StyledRoot = styled.div({
+  border: 'black 1px solid',
+  width: '100%',
+  height: '400px',
+  position: 'relative',
+  padding: 0
+})
 
-  const StyledRoot = styled.div({
-    border: 'black 1px solid',
-    width: '100%',
-    height: '400px',
-    position: 'relative'
-  })
+const DominoWindow = () => {
+  const centerX = 500
+  const centerY = 200
+  const [nowX, setNowX] = React.useState(0)
+  const [nowY, setNowY] = React.useState(0)
+
+  const onClick = () => {
+    setNowX(nowX + 15)
+  }
+
+  const onHover = () => {
+    setNowY(-20)
+  }
+
+  const onBlur = () => {
+    setNowY(0)
+  }
 
   const p1: DominoPlacement = {
-    xPos: centerX,
-    yPos: centerY,
-    size: 300,
-    rotation: 0
+    startingX: centerX,
+    startingY: centerY,
+    currentX: nowX,
+    currentY: nowY,
+    size: 100,
+    rotation: 0,
+    duration: 0.2
   }
 
   return (
     <StyledRoot>
-      <Domino placement={p1} type="3-4" />
+      <Domino
+        onBlur={onBlur}
+        onClick={onClick}
+        onHover={onHover}
+        placement={p1}
+        type="3-4"
+      />
     </StyledRoot>
   )
 }
