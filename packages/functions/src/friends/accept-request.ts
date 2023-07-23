d4b94db73b7717db5a83c0104c5d3eff83c9f3f3
Owen Dunston <owsoni2000@aol.com>
2023-07-23 16:28:45 -0500

    adds accepters name to requester friends, syntax

--- packages/functions/src/friends/accept-request.ts
+++ packages/functions/src/friends/accept-request.ts
@@ -18,23 +18,25 @@ export const main = handler(async (event: any) =>
 
     //get yourself as string and user object
     const accepter = await getCurrentUser(event)
-    const me_fr_fr = event.requestContext.authorizer.iam.cognitoIdentity.amr[2].slice(-36);
+    const me_fr_fr = accepter.username
 
-    //creates copy of current friend list and incoming_friend_requests
+    //creates copy of current friend list and incoming_friend_requests, and new_friends friend list
     let my_friend_list = accepter.friends
     let my_friend_requests = accepter.incoming_friend_requests
+    let their_friend_list = new_friend.friends
 
     //check if new friend's username is in current user's incoming friend requests.
     if (!accepter.incoming_friend_requests.includes(you)) {
         throw new Error("This person has not sent you a friend request");
     }
-    
+
     //adds new_friend username to that list and removes friend request
     my_friend_list.push(new_friend.username)
+    their_friend_list.push(accepter.username)
     const ind = my_friend_requests.indexOf(new_friend.username)
     let my_new_friend_requests = my_friend_requests.splice(ind, 1)
 
-    const params=
+    const my_params=
     {
         TableName: Table.UserInfo.tableName,
         Key: {
@@ -42,13 +44,26 @@ export const main = handler(async (event: any) =>
         },
         UpdateExpression: "SET friends = :friends, incoming_friend_requests = :incoming_friend_requests",
         ExpressionAttributeValues: {
-            //make databse request to set friends equal to new_list
             ":friends": my_friend_list,
-            ":incoming_friend_requests": my_new_friend_requests,
+            ":incoming_friend_requests": my_friend_requests,
+        },
+        ReturnValues: "ALL_NEW",
+    };
+
+    const their_params=
+    {
+        TableName: Table.UserInfo.tableName,
+        Key: {
+            user_id: new_friend.user_id,
+        },
+        UpdateExpression: "SET friends = :friends",
+        ExpressionAttributeValues: {
+            ":friends": their_friend_list,
         },
         ReturnValues: "ALL_NEW",
     };
     
-    await dynamoDB.update(params);
+    await dynamoDB.update(my_params);
+    await dynamoDB.update(their_params);
     return {status: true};
 })
