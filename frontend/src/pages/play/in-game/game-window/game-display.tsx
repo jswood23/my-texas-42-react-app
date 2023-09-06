da12fc3207cc652814fcecc5f0ec6553e5ad006a
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-09-06 12:23:18 -0500

    start game display window

---
+++ frontend/src/pages/play/in-game/game-window/game-display.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, MenuItem, Select, TextField, Typography } from '@mui/material'
+import { type GlobalObj } from '../../../../types'
+import * as React from 'react'
+import Domino from '../../../../shared/domino'
+import styled from 'styled-components'
+import { THEME } from '../../../../constants/theme'
+import ShowDominoes from './show-dominoes'
+
+const StyledRoot = styled.div(({ theme }) => ({
+  position: 'relative',
+  '.player-move-box': {
+    border: '1px solid #000000',
+    height: theme.spacing(15),
+    overflowY: 'auto',
+    padding: theme.spacing(1)
+  }
+}))
+
+interface Props {
+  globals: GlobalObj
+}
+
+const GameDisplay = ({ globals }: Props) => {
+  const gameWindowWidth = +(THEME.spacing(77.5).slice(0, -2))
+  const gameWindowHeight = +(THEME.spacing(67).slice(0, -2))
+
+  const [showGrid, setShowGrid] = React.useState(false)
+
+  const displayGrid = () => {
+    const horizontalLines = () => {
+      const spacing = gameWindowHeight / 10
+      return Array.from(Array(10).keys()).map(i => <line key={`hline-${i}`} y1={`${i * spacing}px`} x1='0px' y2={`${i * spacing}px`} x2 = {`${gameWindowWidth}px`} />)
+    }
+    const verticalLines = () => {
+      const spacing = gameWindowWidth / 10
+      return Array.from(Array(10).keys()).map(i => <line key={`vline-${i}`} x1={`${i * spacing}px`} y1='0px' x2={`${i * spacing}px`} y2 = {`${gameWindowHeight}px`} />)
+    }
+    return (
+      <svg width={gameWindowWidth} height={gameWindowHeight} fill='black' stroke='black' strokeWidth={0.1}>
+        {horizontalLines()}
+        {verticalLines()}
+      </svg>
+    )
+  }
+
+  return (
+    <StyledRoot>
+      {showGrid && displayGrid()}
+      <ShowDominoes globals={globals} newRound={true} windowHeight={gameWindowHeight} windowWidth={gameWindowWidth} />
+    </StyledRoot>
+  )
+}
+
+export default GameDisplay
