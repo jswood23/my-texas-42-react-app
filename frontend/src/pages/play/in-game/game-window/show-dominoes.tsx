db5114c8e5976feccb44530a5e9544b2d9a58012
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-09-06 12:19:52 -0500

    add show dominoes component

---
+++ frontend/src/pages/play/in-game/game-window/show-dominoes.tsx
@@ -0,0 +1,52 @@
+import { type DominoPlacement, type GlobalObj } from '../../../../types'
+import * as React from 'react'
+import Domino from '../../../../shared/domino'
+import { getShuffledDominoes, getStartingDominoes } from './utils/determine-domino-locations'
+
+interface Props {
+  globals: GlobalObj
+  newRound: boolean
+  windowHeight: number
+  windowWidth: number
+}
+
+const ShowDominoes = ({ globals, newRound = false, windowHeight, windowWidth }: Props) => {
+  const [dealDominoes, setDealDominoes] = React.useState(false)
+  const [dominoes, setDominoes] = React.useState([] as DominoPlacement[])
+
+  React.useEffect(() => {
+    if (newRound) {
+      setDominoes(getShuffledDominoes(windowWidth, windowHeight))
+      setDealDominoes(true)
+    }
+  }, [newRound])
+
+  React.useEffect(() => {
+    if (dealDominoes) {
+      setDominoes(getStartingDominoes(windowWidth, windowHeight))
+      setDealDominoes(false)
+    }
+  }, [dealDominoes, setDealDominoes])
+
+  const displayDominoes = () => {
+    let i = 0
+    return dominoes.map(domino => {
+      i += 1
+      return (
+      <Domino
+        key={`domino-${i}`}
+        placement={domino}
+        type='6-1'
+      />
+      )
+    })
+  }
+
+  return (
+    <>
+      {displayDominoes()}
+    </>
+  )
+}
+
+export default ShowDominoes
