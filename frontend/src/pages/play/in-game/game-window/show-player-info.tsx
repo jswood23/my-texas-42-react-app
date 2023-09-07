6ba133b159ba18e6471decc1e3caa26c9c76be79
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-09-07 15:05:43 -0500

    add show player info component

---
+++ frontend/src/pages/play/in-game/game-window/show-player-info.tsx
@@ -0,0 +1,17 @@
+import { type GlobalObj } from '../../../../types'
+import * as React from 'react'
+
+interface Props {
+  globals: GlobalObj
+  windowHeight: number
+  windowWidth: number
+}
+
+const ShowPlayerInfo = ({ globals, windowHeight, windowWidth }: Props) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default ShowPlayerInfo
