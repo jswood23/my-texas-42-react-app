11618f6782eb7abd97e95b7123564ecab3794ad8
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-09-16 18:08:41 -0500

    add server message and loading spinner

--- frontend/src/pages/play/in-game/game-window/show-player-options/show-play-button.tsx
+++ frontend/src/pages/play/in-game/game-window/show-player-options/show-play-button.tsx
@@ -1,7 +1,9 @@
 import type { DominoObj, GlobalObj } from '../../../../../types'
+import { MOVE_TYPES } from '../../../../../constants/game-constants'
 import { pos } from '../utils/helpers'
-import GameButton from '../../../../../shared/game-button'
 import * as React from 'react'
+import GameButton from '../../../../../shared/game-button'
+import GameSpinner from '../../../../../shared/game-spinner'
 
 interface Props {
   globals: GlobalObj
@@ -12,24 +14,41 @@ interface Props {
 }
 
 const ShowPlayButton = ({ globals, windowHeight, windowWidth, stagedDomino, setStagedDomino }: Props) => {
+  const [hasPlayed, setHasPlayed] = React.useState(false)
   const disablePlayButton = React.useMemo(() => { return !stagedDomino }, [stagedDomino])
 
   const onClickPlay = () => {
-    console.log('click me')
+    globals.connection.sendJsonMessage({ action: 'play_turn', data: JSON.stringify({ move: stagedDomino?.type, moveType: MOVE_TYPES.play }) })
+    setHasPlayed(true)
+  }
+
+  const showGameSpinner = () => {
+    return (
+      <>
+        <GameSpinner
+          xpos={pos(50, windowWidth)}
+          ypos={pos(50, windowHeight)}
+          size={pos(5, windowWidth)}
+        />
+      </>
+    )
   }
 
   return (
     <>
-      <GameButton
-        xpos={pos(44, windowWidth)}
-        ypos={pos(64.5, windowHeight)}
-        width={pos(12, windowWidth)}
-        height={pos(6, windowHeight)}
-        text='Play'
-        fontSize={pos(2, windowWidth)}
-        disabled={disablePlayButton}
-        onClick={onClickPlay}
-      />
+      {hasPlayed
+        ? showGameSpinner()
+        : <GameButton
+          xpos={pos(44, windowWidth)}
+          ypos={pos(64.5, windowHeight)}
+          width={pos(12, windowWidth)}
+          height={pos(6, windowHeight)}
+          text='Play'
+          fontSize={pos(2, windowWidth)}
+          disabled={disablePlayButton}
+          onClick={onClickPlay}
+        />
+      }
     </>
   )
 }
