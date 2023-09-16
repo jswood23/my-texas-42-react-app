5c136409052ae85d987dbb128b951f18c3e6a780
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-09-16 12:50:35 -0500

    add game spinner after bidding

--- frontend/src/pages/play/in-game/game-window/show-player-options/show-bidding-options.tsx
+++ frontend/src/pages/play/in-game/game-window/show-player-options/show-bidding-options.tsx
@@ -5,6 +5,7 @@ import GameButton from '../../../../../shared/game-button'
 import { pos } from '../utils/helpers'
 import { MOVE_TYPES } from '../../../../../constants/game-constants'
 import GameNumberPicker from '../../../../../shared/game-number-picker'
+import GameSpinner from '../../../../../shared/game-spinner'
 
 interface Props {
   globals: GlobalObj
@@ -13,6 +14,7 @@ interface Props {
 }
 
 const ShowBiddingOptions = ({ globals, windowHeight, windowWidth }: Props) => {
+  const [hasBid, setHasBid] = React.useState(false)
   const currentHighestBid = React.useMemo(() => getCurrentHighestBid(globals.gameState.current_round_history), [globals.gameState.current_round_history])
   const showNumericBid = currentHighestBid < 42
   const markBid = showNumericBid ? 84 : currentHighestBid + 42
@@ -26,44 +28,66 @@ const ShowBiddingOptions = ({ globals, windowHeight, windowWidth }: Props) => {
 
   const makeBid = (bid: number) => {
     globals.connection.sendJsonMessage({ action: 'play_turn', data: JSON.stringify({ move: bid, moveType: MOVE_TYPES.bid }) })
+    setHasBid(true)
+  }
+
+  const showButtons = () => {
+    return (
+      <>
+        <GameButton
+          xpos={pos(xPositions[0], windowWidth)}
+          ypos={pos(60, windowHeight)}
+          width={pos(12, windowWidth)}
+          height={pos(6, windowHeight)}
+          fontSize={pos(2, windowWidth)}
+          text={getBidString(0)}
+          onClick={() => {
+            makeBid(0)
+          }}
+        />
+        {showNumericBid &&
+          <GameNumberPicker
+            xpos={pos(xPositions[1], windowWidth)}
+            ypos={pos(60, windowHeight)}
+            width={pos(18, windowWidth)}
+            height={pos(6, windowHeight)}
+            fontSize={pos(3, windowWidth)}
+            onChoose={makeBid}
+            min={lowestAllowedBid}
+            max={42}
+          />
+        }
+        <GameButton
+          xpos={pos(xPositions[2], windowWidth)}
+          ypos={pos(60, windowHeight)}
+          width={pos(12, windowWidth)}
+          height={pos(6, windowHeight)}
+          fontSize={pos(2, windowWidth)}
+          text={getBidString(markBid)}
+          onClick={() => {
+            makeBid(markBid)
+          }}
+        />
+      </>
+    )
+  }
+
+  const showGameSpinner = () => {
+    return (
+      <>
+        <GameSpinner
+          xpos={pos(50, windowWidth)}
+          ypos={pos(50, windowHeight)}
+          size={pos(5, windowWidth)}
+        />
+      </>
+    )
   }
 
   return (
     <>
-      <GameButton
-        xpos={pos(xPositions[0], windowWidth)}
-        ypos={pos(60, windowHeight)}
-        width={pos(12, windowWidth)}
-        height={pos(6, windowHeight)}
-        fontSize={pos(2, windowWidth)}
-        text={getBidString(0)}
-        onClick={() => {
-          makeBid(0)
-        }}
-      />
-      {showNumericBid &&
-        <GameNumberPicker
-          xpos={pos(xPositions[1], windowWidth)}
-          ypos={pos(60, windowHeight)}
-          width={pos(18, windowWidth)}
-          height={pos(6, windowHeight)}
-          fontSize={pos(3, windowWidth)}
-          onChoose={makeBid}
-          min={lowestAllowedBid}
-          max={42}
-        />
-      }
-      <GameButton
-        xpos={pos(xPositions[2], windowWidth)}
-        ypos={pos(60, windowHeight)}
-        width={pos(12, windowWidth)}
-        height={pos(6, windowHeight)}
-        fontSize={pos(2, windowWidth)}
-        text={getBidString(markBid)}
-        onClick={() => {
-          makeBid(markBid)
-        }}
-      />
+      {!hasBid && showButtons()}
+      {hasBid && showGameSpinner()}
     </>
   )
 }
