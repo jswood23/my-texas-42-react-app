7aa3029a30d07ab14556c83e887b58d6ebc7ea00
jswood23 <56269953+jswood23@users.noreply.github.com>
2024-03-28 00:16:02 -0500

    temporarily disable variant rules

--- frontend/src/constants/rules.ts
+++ frontend/src/constants/rules.ts
@@ -14,41 +14,41 @@ export const RULES: Rule[] = [
       'If the first three players pass, the fourth player must bid 31 or higher.',
     excludes: ['No forced bid'],
     requires: []
-  },
-  {
-    rule_name: 'Forced Nil',
-    rule_description:
-      'If a player is forced to bid, they have the option to bid 1-mark Nil.',
-    excludes: ['No forced bid'],
-    requires: []
-  },
-  {
-    rule_name: 'Nil',
-    rule_description:
-      'A player may bid 2 marks and choose Nil as an option. They may also choose doubles-are-high, doubles-are-low, or doubles-are-their-own-suit.',
-    excludes: [],
-    requires: []
-  },
-  {
-    rule_name: 'Splash',
-    rule_description:
-      'A player may bid 2 marks and choose Splash as an option. The player must have at least 3 doubles in their hand.',
-    excludes: [],
-    requires: []
-  },
-  {
-    rule_name: 'Plunge',
-    rule_description:
-      'A player may bid 2 marks and choose Plunge as an option. The player must have at least 4 doubles in their hand. Upon choosing plunge, the bid is raised by one mark.',
-    excludes: [],
-    requires: ['Splash']
-  },
-  {
-    rule_name: 'Sevens',
-    rule_description:
-      'A player may bid 2 marks and choose Sevens as an option.',
-    excludes: [],
-    requires: []
+  // },
+  // {
+  //   rule_name: 'Forced Nil',
+  //   rule_description:
+  //     'If a player is forced to bid, they have the option to bid 1-mark Nil.',
+  //   excludes: ['No forced bid'],
+  //   requires: []
+  // },
+  // {
+  //   rule_name: 'Nil',
+  //   rule_description:
+  //     'A player may bid 2 marks and choose Nil as an option. They may also choose doubles-are-high, doubles-are-low, or doubles-are-their-own-suit.',
+  //   excludes: [],
+  //   requires: []
+  // },
+  // {
+  //   rule_name: 'Splash',
+  //   rule_description:
+  //     'A player may bid 2 marks and choose Splash as an option. The player must have at least 3 doubles in their hand.',
+  //   excludes: [],
+  //   requires: []
+  // },
+  // {
+  //   rule_name: 'Plunge',
+  //   rule_description:
+  //     'A player may bid 2 marks and choose Plunge as an option. The player must have at least 4 doubles in their hand. Upon choosing plunge, the bid is raised by one mark.',
+  //   excludes: [],
+  //   requires: ['Splash']
+  // },
+  // {
+  //   rule_name: 'Sevens',
+  //   rule_description:
+  //     'A player may bid 2 marks and choose Sevens as an option.',
+  //   excludes: [],
+  //   requires: []
   // },
   // {
   //   rule_name: 'Delve',
